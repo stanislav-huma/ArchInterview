6616e351d4cc08fa69907f1734c13d9682d3cd98
ashcenso <ashcenso@gmail.com>
2023-11-17 13:13:34 +0200

    Setup project

---
+++ ArchInterview/ArchInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchInterviewApp.swift
+//  ArchInterview
+//
+//  Created by Ash Censo on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ArchInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
